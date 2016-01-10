6601245dbbc492aad961c0d69c841c16cd6c4f56
oliverswitzer <oliverswitzer@gmail.com>
2016-01-10 17:43:42 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoOrDie::Application.config.secret_key_base = '06e69194487d6def87674913fd4c1c77a0e8b3ee832e2618d6b1f973b117029fa0c590aaa8dfd740639c103991cc0d1361e74535af3855a63466d2486007b918'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoOrDie::Application.config.session_store :cookie_store, key: '_todo-or-die_session'
